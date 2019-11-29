82114f4a560b534b918e70b36ebe27ed28ca9da4
jvhariharan <jvhariharan@gmail.com>
2019-11-29 10:58:10 +0530

    Edit all rows in batch editing

---
+++ EJ2MVCEditAllRows/EJ2MVC/Controllers/HomeController.cs
@@ -0,0 +1,100 @@
+﻿using EJ2MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EJ2MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+            var order = OrdersDetails.GetAllRecords();
+            ViewBag.datasource = order;
+            return View();
+        }
+
+
+        public ActionResult UrlDatasource(Data dm)
+        {
+            var order = OrdersDetails.GetAllRecords();
+            var Data = order.ToList();
+            int count = order.Count();
+            return dm.requiresCounts ? Json(new { result = Data.Skip(dm.skip).Take(dm.take), count = count }) : Json(Data);
+        }
+
+
+        public ActionResult Update(OrdersDetails value)
+        {
+            var ord = value;
+            OrdersDetails val = OrdersDetails.GetAllRecords().Where(or => or.OrderID == ord.OrderID).FirstOrDefault();
+            val.OrderID = ord.OrderID;
+            val.EmployeeID = ord.EmployeeID;
+            val.CustomerID = ord.CustomerID;
+            string msg = "Successfully performed editing the record";
+            return Json(new { data = value, message = msg });
+        }
+        //insert the record
+        public ActionResult Insert(OrdersDetails value)
+        {
+            var ord = value;
+            var add = OrdersDetails.GetAllRecords().Where(or => or.OrderID == ord.OrderID).FirstOrDefault();
+            if (add == null)
+            {
+                OrdersDetails.GetAllRecords().Insert(0, value);
+                string msg = "Successfully performed inserting a record";
+                return Json(new { data = value, message = msg });
+            }
+            else
+            {
+                string msg = "duplicates acnnot be inserted";
+                return Json(new { data = value, message = msg });
+            }
+        }
+        //Delete the record
+        public ActionResult Delete(OrdersDetails value)
+        {
+            OrdersDetails.GetAllRecords().Remove(OrdersDetails.GetAllRecords().Where(or => or.OrderID == value.OrderID).FirstOrDefault());
+            string msg = "Successfully performed deleting a record";
+            return Json(new { data = value, message = msg });
+        }
+
+        public ActionResult getStatus(OrdersDetails value) {
+
+            var ord = value;
+            var add = OrdersDetails.GetAllRecords().Where(or => or.OrderID == ord.OrderID).FirstOrDefault();
+            if (add != null)
+            {
+                return Content("false duplicates acnnot be inserted");
+            }            
+            return Content("true");
+        }
+
+        public class Data
+        {
+
+            public bool requiresCounts { get; set; }
+            public int skip { get; set; }
+            public int take { get; set; }
+            public int value { get; set; }
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ EJ2MVCEditAllRows/EJ2MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EJ2MVC.Startup))]
+namespace EJ2MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
